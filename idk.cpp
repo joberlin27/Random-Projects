38ecf12008a340ca387653be59bdc4b73d87d026
Josh Oberlin <josh.oberlin@apexorderpickup.com>
2025-09-09 15:57:35 -0400

    hi

---
+++ idk.cpp
@@ -0,0 +1,12 @@
+// Joshua Oberlin
+// 8-14-2025
+
+#include <iostream>
+
+int main(){
+
+    std::cout<<"This was a pain" << std::endl;
+
+    std::cout<<"Why does microsoft make everything so difficult....";
+
+}
